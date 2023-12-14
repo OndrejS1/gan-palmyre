550268dae2adb9a95864f123e36859dd52561e05
Ondřej Svojše <ondrej.svojse@gendigital.com>
2023-12-14 14:31:03 +0100

    Fix the prediction

--- app.py
+++ app.py
@@ -63,60 +63,54 @@ def predict():
     return make_response(result, 200)
 
 
-def predict_with_weight_and_architecture(architecture, weights, classes, img_size):
-    # loading trained model architecture and weights from saved file
-    json_file = open(architecture, 'r')
-    loaded_model_json = json_file.read()
-    json_file.close()
+def predict_with_weight_and_architecture(architecture, weights, classes_file, img_size):
+    # Load trained model architecture and weights
+    with open(architecture, 'r') as json_file:
+        loaded_model_json = json_file.read()
     loaded_model = model_from_json(loaded_model_json)
     loaded_model.load_weights(weights)
 
-    # file = flask.request.files['inputFile']
-    # img = Image.open(file.stream)
-    # Open Class labels dictionary. (human readable label given ID, alphabetically ordered, just as the classes were trained)
-    classes = eval(open(classes, 'r').read())
-    print(request.form['imageBase64'])
+    # Open Class labels dictionary
+    with open(classes_file, 'r') as class_file:
+        classes = eval(class_file.read())
+
+    # Extract and preprocess the image
     image_data = re.sub('^data:image/.+;base64,', '', request.form['imageBase64'])
     img = Image.open(BytesIO(base64.b64decode(image_data)))
-    img.save("input" + str(randrange(1000)) + "__" + str(img_size) + "x" + str(img_size) + ".png")
-    # img = img.resize((img_size, img_size), Image.BILINEAR)
-    img = np.array(img)
-    img = cv2.bitwise_not(img)
-    img = cv2.cvtColor(img, cv2.COLOR_RGBA2BGR)
 
-    # changing format to neural network readable and normalisation
-    img = img.reshape((1, img.shape[0], img.shape[1], img.shape[2]))
+    # Resize the image to the expected input size
+    img = img.resize((img_size, img_size), Image.BILINEAR)
+    img = np.array(img)
+
+    # Ensure the image has 3 channels (BGR format)
+    if img.shape[-1] == 4:  # Convert RGBA to BGR
+        img = cv2.cvtColor(img, cv2.COLOR_RGBA2BGR)
+    elif img.shape[-1] == 1:  # Convert grayscale to BGR if needed
+        img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+
+    # Normalize the image
     img = img.astype('float32') / 255
 
-    # running prediction on test image
+    # Add a batch dimension to the image
+    img = np.expand_dims(img, axis=0)
+
+    # Run prediction on the processed image
     preds = loaded_model.predict(img)
 
+    # Process prediction results
     pred_result = dict(zip(range(len(preds[0])), preds[0]))
-
     sorted_result = sorted(pred_result.items(), key=operator.itemgetter(1), reverse=True)
 
-    # print(preds)
-    # print(np.argmax(preds[0], axis=0))
+    results = []
+    for i in range(min(3, len(sorted_result))):
+        identified_class = classes[sorted_result[i][0]]
+        probability = str(sorted_result[i][1])
+        results.append({'class': identified_class, 'probability': probability})
 
-    identified_class_1 = classes[sorted_result[0][0]]
-    probability_1 = str(sorted_result[0][1])
+    # Print the top 3 predictions
+    print(*results, sep="\n")
 
-    identified_class_2 = classes[sorted_result[1][0]]
-    probability_2 = str(sorted_result[1][1])
-
-    identified_class_3 = classes[sorted_result[2][0]]
-    probability_3 = str(sorted_result[2][1])
-
-    print(  {'class': identified_class_1, 'probability': probability_1},
-            {'class': identified_class_2, 'probability': probability_2},
-            {'class': identified_class_3, 'probability': probability_3})
-
-    return [
-        {'class': identified_class_1, 'probability': probability_1},
-        {'class': identified_class_2, 'probability': probability_2},
-        {'class': identified_class_3, 'probability': probability_3}
-    ]
-
+    return results
 
 if __name__ == '__main__':
     app.run()
